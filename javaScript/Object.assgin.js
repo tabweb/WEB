01dfee8163da04008bb083064edf77d2a4314678
tabweb <tabweb@163.com>
2019-08-07 19:24:27 +0800

    xxx

--- javaScript/Object.assgin.js
+++ javaScript/Object.assgin.js
@@ -13,40 +13,102 @@ var new_arr = JSON.parse(JSON.stringify(arr));
 */
 
 {
-    function shllow(obj) {
-        if (typeof obj !== 'object') return;
-        var newObj = obj instanceof Array ? [] : {};
-        for (key in obj) {
-            console.log(key)
-            if (obj.hasOwnProperty(key)) {
-                newObj[key] = obj[key];
-            }
-        }
-        return newObj;
-    }
+	function shllow(obj) {
+		if (typeof obj !== 'object') return;
+		var newObj = obj instanceof Array ? [] : {};
+		for (key in obj) {
+			console.log(key)
+			if (obj.hasOwnProperty(key)) {
+				newObj[key] = obj[key];
+			}
+		}
+		return newObj;
+	}
 
-    let obj = {a: 1, b: {x: 2, c: {y: 3}}}
-    let newObj = shllow(obj);
-    newObj.a = 'a'
-    newObj.b.x = 'x';
-    newObj.b.c.y = 'z';
-    console.log(obj);
-    console.log(newObj);
+	let obj = {
+		a: 1,
+		b: {
+			x: 2,
+			c: {
+				y: 3
+			}
+		}
+	}
+	let newObj = shllow(obj);
+	newObj.a = 'a'
+	newObj.b.x = 'x';
+	newObj.b.c.y = 'z';
+	console.log(obj);
+	console.log(newObj);
 }
 //
 {
-    let a = {name: 'FE'};
-    let b = a;
-    b.name = 'EF';
-    console.log(a.name) // EF
+	let a = {
+		name: 'FE'
+	};
+	let b = a;
+	b.name = 'EF';
+	console.log(a.name) // EF
+} {
+	// 浅拷贝
+	// Object.assign , ...(展开运算符)
+	let a = {
+		age: 1
+	}
+	let b = Object.assign({}, a);
+	a.age = 2;
+	console.log(b.age) // 1
+
+	// so copy
+	// object.assign
+	const target = {
+		a: 1
+	}
+	const source1 = {
+		b: 2
+	}
+	const source2 = {
+		c: 3
+	}
+	Object.assign(target, source1, source2)
+	console.log(target) // {a: 1, b: 2, c: 3}
 }
+
+
+
+// https://github.com/nearxu/have-fun-js/blob/master/es5/type.js
+// 浅赋值 深赋值
 {
-    // 浅拷贝
-    // Object.assign , ...(展开运算符)
-    let a = {
-        age: 1
+    // deepCopy
+	const deepCopy = (obj) => {
+		var newobj = {};
+		for (let key in obj) {
+			if (obj.hasOwnProperty(key)) {
+				if (typeof (key) === 'object') {
+					newobj[key] = deepCopy(obj[key])
+				} else {
+					newobj[key] = obj[key]
+				}
+			}
+		}
+		return newobj
+	}
+	const ab = {
+		name: 'tom'
+	}
+	const cd = deepCopy(ab)
+	cd.name = 'marry'
+    console.log(ab)
+
+    // solute2
+    const deepKeys = obj => {
+    	let clone = Object.assign({}, obj);
+    	Object.keys(clone).forEach(
+    		k => (clone[k] = typeof (obj[k]) === 'object' ? deepKeys(obj[k]) : obj[k])
+    	)
+    	return clone
     }
-    let b = Object.assign({}, a);
-    a.age = 2;
-    console.log(b.age) // 1
+    const ef = deepKeys(cd)
+    ef.name = 'near'
+    console.log(cd)
 }
--- javaScript/Object.js
+++ javaScript/Object.js
@@ -30,3 +30,28 @@
 */
 
 
+
+
+
+/*
+1、使用ES6提供的 Object.keys(obj) 方法
+Object.keys 返回一个数组，包括对象自身的（不含继承的）所有可枚举属性（不含 Symbol 属性）的键名。
+
+Object.keys(obj).length === 0 && obj.constructor === Object  // true表示为空对象，false为非空对象
+缺点： 部分浏览器不支持，需要通过 babel 类插件转为 ES5
+
+2、遍历对象，通过对象的 hasOwnProperty() 方法判断
+function isEmptyObject( obj ) {
+    var name;
+    for ( name in obj ) {
+        return false;
+    }
+    return true;
+}
+3、jQuery.isEmptyObject()，源码见上方；
+*/
+
+
+
+
+
